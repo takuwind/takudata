795db378c73932c4d2500810e95c3c82b4b95b23
takuwind <as9214237@gmail.com>
2014-02-23 22:37:09 +0800

    week1

---
+++ week1.java
@@ -0,0 +1,10 @@
+public class week1 {
+  public static void main(String args[])  { 
+  
+	System.out.println("桃花溪 -- 唐．張旭\n\n");
+	System.out.println("隱隱飛橋隔野煙，\n");
+	System.out.println("石磯西畔問漁船。\n");
+	System.out.println("桃花盡日隨流水，\n");
+	System.out.println("洞在清谿何處邊？\n");
+  }
+}
